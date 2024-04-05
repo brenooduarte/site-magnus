0cae0649decbf3ce5756501e3bc0ad87c5ff32ab
brenooduarte <brenoo.duarte@hotmail.com>
2024-04-05 16:44:31 -0300

    feat: add navigation between items manually

--- src/components/TestimonialSlide/index.jsx
+++ src/components/TestimonialSlide/index.jsx
@@ -1,5 +1,9 @@
 import "./styles.scss";
 import Card from "../TestimonialCard/index.jsx";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/scss";
+import "swiper/scss/pagination";
 
 const CARDS = 3;
 
@@ -10,17 +14,22 @@ const TestimonialSlide = ({
   customerTestimonials,
   customerBrands,
 }) => {
-  const cardList = Array.from({ length: CARDS }, (_, index) => (
-    <Card
-      key={index}
-      photo={customerPhotos[index]}
-      name={customerNames[index]}
-      company={customerCompanies[index]}
-      testimonial={customerTestimonials[index]}
-      brand={customerBrands[index]}
-    />
-  ));
-
+  const cardList = (
+    <Swiper spaceBetween={50} slidesPerView={1} speed={1200} navigation={true}>
+      {Array.from({ length: CARDS }, (_, index) => (
+        <SwiperSlide key={index}>
+          <Card
+            key={index}
+            photo={customerPhotos[index]}
+            name={customerNames[index]}
+            company={customerCompanies[index]}
+            testimonial={customerTestimonials[index]}
+            brand={customerBrands[index]}
+          />
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
   return <div className="testimonial-slide">{cardList}</div>;
 };
 
